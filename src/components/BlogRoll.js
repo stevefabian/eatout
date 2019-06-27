f01b6ed8a54f5d057fca6441fc50934b7a62aab7
Steve Fabian <steve.fabian@gooddogs.com>
2019-06-27 07:51:48 -0400

    updated branding and ui

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -12,25 +12,13 @@ class BlogRoll extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
+            <div className="is-parent column is-4" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? 'is-featured' : ''
                   }`}
               >
                 <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${
-                            post.title
-                            }`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
                   <p className="post-meta">
                     <div><Link
                       className="title has-text-primary is-size-5"
